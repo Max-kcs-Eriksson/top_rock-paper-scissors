fc3daf27eaeb4128f1e607bd185f6ae956913ef6
Max <max.kcs.eriksson@hotmail.com>
2022-06-23 00:43:12 +0200

    Add pseudocode to guide further progress

---
+++ projects/rock-paper-scissors/javascript.js
@@ -0,0 +1,15 @@
+// Select rock paper scissor
+//     Player selection
+//         User writes their choice in a prompt
+//             case-insensitive
+//             convert player input string to lower case
+//     Computer select
+//     randomly generated selection
+// Round 1
+//     declare winner
+//         Compare playerSelection and computerSelection
+//     keep score
+// Round 2 - 4
+// Round 5
+//     Declare final Winner
+//     Reset
