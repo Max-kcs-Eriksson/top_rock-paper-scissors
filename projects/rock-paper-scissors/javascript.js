ade4afbf48088d547d5f0b2dcf067abb47ba8953
Max <max.kcs.eriksson@hotmail.com>
2022-06-23 01:51:55 +0200

    Add function to get random integer
    Greater than or equal to min, and lower than or equal to max.

--- projects/rock-paper-scissors/javascript.js
+++ projects/rock-paper-scissors/javascript.js
@@ -4,7 +4,12 @@
 //             case-insensitive
 //             convert player input string to lower case
 //     Computer select
-//     randomly generated selection
+//         randomly generated selection
+function computerPlay() {
+    // generate random integer
+    // assign numerical values to rock paper scissor
+}
+
 // Round 1
 //     declare winner
 //         Compare playerSelection and computerSelection
@@ -13,3 +18,14 @@
 // Round 5
 //     Declare final Winner
 //     Reset
+
+
+// FUNCTIONS
+
+// Returns a random integer between min (inclusive) and max (inclusive).
+// SOURCE: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
