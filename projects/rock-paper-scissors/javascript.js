8947ce530e2ea4637b656454fa9a7da34d3757ff
Max <max.kcs.eriksson@hotmail.com>
2022-06-23 03:46:01 +0200

    Add function to get random selection from opponent
    The function computerPlay() generates a number and returns the variable computerSelection containing a string of either rock, paper, or scissors. See line 12 - 34.
    The function is called further down and computerSelection is printed to the console. See line 46 - 47.

--- projects/rock-paper-scissors/javascript.js
+++ projects/rock-paper-scissors/javascript.js
@@ -1,25 +1,3 @@
-// Select rock paper scissor
-//     Player selection
-//         User writes their choice in a prompt
-//             case-insensitive
-//             convert player input string to lower case
-//     Computer select
-//         randomly generated selection
-function computerPlay() {
-    // generate random integer
-    // assign numerical values to rock paper scissor
-}
-
-// Round 1
-//     declare winner
-//         Compare playerSelection and computerSelection
-//     keep score
-// Round 2 - 4
-// Round 5
-//     Declare final Winner
-//     Reset
-
-
 // FUNCTIONS
 
 // Returns a random integer between min (inclusive) and max (inclusive).
@@ -29,3 +7,49 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Returns a randomly generated selection.
+let computerSelection;
+function computerPlay() {
+    // Generate random integer.
+    // GetRandomInt() is defined on line 5 - 9.
+    let randomValue = getRandomInt(1, 3);
+
+    // Assign numerical values to rock paper scissors.
+    let rock = 1;
+    let paper = 2;
+    let scissors = 3;
+
+    // Transform randomValue to rock paper scissors.
+    if (randomValue === rock) {
+        computerSelection = 'Computer chose: rock';
+    } else if (randomValue === paper) {
+        computerSelection = 'Computer chose: paper';
+    } else if (randomValue === scissors) {
+        computerSelection = 'Computer chose: scissors';
+    } else {
+        alert('Computer didn\'t choose');
+    }
+    return computerSelection;
+}
+
+
+// GAME EXECUTION
+
+// Select rock paper scissor
+//     Player selection
+let playerSelection;
+//         User writes their choice in a prompt
+//             case-insensitive
+//             convert player input string to lower case
+//     Computer selection
+computerPlay();
+console.log(computerSelection);
+// Round 1
+//     declare winner
+//         Compare playerSelection and computerSelection
+//     keep score
+// Round 2 - 4
+// Round 5
+//     Declare final Winner
+//     Reset
